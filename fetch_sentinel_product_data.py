5eaf76f1dd2ef0d6b50a7a9d6a852c708d9cc49c
vetleras <vetleras@gmail.com>
2023-02-15 10:40:05 +0100

    Create fetch_sentinel_product_data

---
+++ fetch_sentinel_product_data.py
@@ -0,0 +1,30 @@
+import pathlib
+import click
+from geopy.geocoders import Nominatim
+from sentinelsat import SentinelAPI
+from shapely import Point
+
+GEOLOCATOR = Nominatim(user_agent="NTNU TTK4852 group 1")
+API = SentinelAPI("vetlerastirsed-javru6-nixdAk", "tirsed-javru6-nixdAk")
+
+
+@click.command()
+@click.argument("cities", nargs=-1)
+def main(cities: tuple[str]):
+    """Download product data from Sentinel API to sentinel_product_data"""
+    pathlib.Path(f"sentinel_product_data").mkdir(exist_ok=True)
+    for city in cities:
+        location = GEOLOCATOR.geocode(city)
+        point = Point((location.latitude, location.longitude))
+        products = API.query(
+            point,
+            area_relation="Contains",
+            platformname="Sentinel-2",
+            cloudcoverpercentage=(0, 30),
+        )
+        df = API.to_dataframe(products)
+        df.to_csv(f"sentinel_product_data/{city}.csv")
+
+
+if __name__ == "__main__":
+    main()
