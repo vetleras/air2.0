5075531c9a41d06ba382af282b239342a5231443
Andreas Gangstad Rygg <andreryg@stud.ntnu.no>
2023-02-22 15:05:47 +0100

    small fix

--- distance_calculation.py
+++ distance_calculation.py
@@ -5,15 +5,17 @@ from shapely import geometry
 def distance_calculation(latitude, longitude):
   p = Proj('EPSG:3035', preserve_units=False)
   x,y = p(latitude, longitude)
-  point1 = [x-10*10**3, y]
+  distance = 10*10**3
+  point1 = [x-distance, y]
   point1_c = p(point1[0], point1[1], inverse=True)
-  point2 = [x, y-10*10**3]
+  point2 = [x, y-distance]
   point2_c = p(point2[0], point2[1], inverse=True)
 
   max_latitude = np.max([np.abs(point1_c[0] - latitude), np.abs(point2_c[0] - latitude)])
   max_longitude = np.max([np.abs(point1_c[1] - longitude), np.abs(point2_c[1] - longitude)])
 
   offset = (max_latitude + max_longitude) / 2
+  #https://stackoverflow.com/questions/30457089/how-to-create-a-shapely-polygon-from-a-list-of-shapely-points
   p1 = geometry.Point(latitude-offset, longitude-offset)
   p2 = geometry.Point(latitude-offset, longitude+offset)
   p3 = geometry.Point(latitude+offset, longitude+offset)
