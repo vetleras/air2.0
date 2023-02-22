dd123de71b8a0b53a610c25aecb6e5184eba53e2
vetleras <vetleras@gmail.com>
2023-02-22 15:07:24 +0100

    Rename and comment

--- aggregate.py
+++ aggregate.py
@@ -11,16 +11,19 @@ def main(city: str):
     """Output the worst dates for a given city which has data in air_quality_data and sentinel_product_data"""
     city_dir_path = os.path.join("air_quality_data", city)
 
-    main_df = pd.DataFrame(columns=["time"])
+    main_df = pd.DataFrame(columns=["measured_at"])
     for filename in os.listdir(city_dir_path):
         df = pd.read_csv(os.path.join(city_dir_path, filename))
-        df = df.rename(columns={"DatetimeBegin": "time", "Concentration": filename})
+        df.rename(
+            columns={"DatetimeBegin": "measured_at", "Concentration": filename},
+            inplace=True,
+        )
         main_df = pd.merge(
-            main_df, df.loc[:, ["time", filename]], on="time", how="outer"
+            main_df, df.loc[:, ["measured_at", filename]], on="measured_at", how="outer"
         )
 
-    main_df["time"] = pd.to_datetime(main_df["time"])
-    df = main_df[["time"]]
+    main_df["measured_at"] = pd.to_datetime(main_df["measured_at"])
+    df = main_df[["measured_at"]]
 
     df["max"] = main_df.max(axis=1, numeric_only=True)
     df = df.loc[df["max"] >= 50]
@@ -29,18 +32,14 @@ def main(city: str):
     product_df = pd.read_csv(os.path.join("sentinel_product_data", f"{city}.csv"))
     images_taken = pd.to_datetime(product_df["beginposition"], utc=True)
 
-    # measured_at, img_taken, max_concentration, time_difference [s]
-    for index, (time, _max) in df.iterrows():
-        image_taken = min(images_taken, key=lambda x: abs(x - time))
+    # df: measured_at, img_taken, max_concentration, time_diff
+    for index, (measured_at, _max) in df.iterrows():
+        image_taken = min(images_taken, key=lambda x: abs(x - measured_at))
         df.at[index, "image_taken"] = image_taken
-        df.at[index, "time_diff"] = abs(time - image_taken)
+        df.at[index, "time_diff"] = abs(measured_at - image_taken)
 
     df = df.loc[df["time_diff"] < datetime.timedelta(days=1)]
     print(df)
-
-
-# large discrepensies between img time and worst dates
-# eo browser does not have the sentinel data
 
 
 if __name__ == "__main__":
