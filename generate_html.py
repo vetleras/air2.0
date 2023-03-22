3e9eaf8fcd42b6bfe2216aec4e800d2941b2498f
Even Kjæxli <112700245+evenkjaexli@users.noreply.github.com>
2023-03-22 12:50:26 +0100

    generate jinja2 templates

--- generate_html.py
+++ generate_html.py
@@ -1,22 +1,26 @@
 from jinja2 import Environment, FileSystemLoader
 
-max_score = 100
-test_name = "Python Challenge"
-students = [
-    {"name": "Sandrine",  "score": 100},
-    {"name": "Gergeley", "score": 87},
-    {"name": "Frieda", "score": 92},
-]
-
 environment = Environment(loader=FileSystemLoader("templates/"))
 
-results_filename = "students_results.html"
+results_filename = "results.html"
 results_template = environment.get_template("results.html")
 context = {
-    "students": students,
-    "test_name": test_name,
-    "max_score": max_score,
-}
+    "city": "Murcia",  
+
+    "date_1" : "2021-03-26", 
+    "date_2" : "2021-04-20", 
+
+    "conc_1" : "50", 
+    "conc_2" : "70", 
+
+    "img_1" : "tmp/Murcia_2021-03-26.jpeg",
+    "img_2" : "tmp/Murcia_2021-04-20.jpeg",
+
+    "prev" : "tmp/Murcia_2021-04-20.jpeg", 
+    "next": "tmp/Murcia_2021-03-26.jpeg",
+
+    }
+
 with open(results_filename, mode="w", encoding="utf-8") as results:
     results.write(results_template.render(context))
     print(f"... wrote {results_filename}")
