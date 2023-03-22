6a4dcd6b8fa9fb73b0fda26c586daff977163d21
Even Kjæxli <112700245+evenkjaexli@users.noreply.github.com>
2023-03-22 13:18:31 +0100

    add colors

--- generate_html.py
+++ generate_html.py
@@ -10,17 +10,16 @@ context = {
     "date_1" : "2021-03-26", 
     "date_2" : "2021-04-20", 
 
-    "conc_1" : "50", 
-    "conc_2" : "70", 
+    "conc_1" : 30, 
+    "conc_2" : 70, 
 
     "img_1" : "tmp/Murcia_2021-03-26.jpeg",
     "img_2" : "tmp/Murcia_2021-04-20.jpeg",
 
     "prev" : "tmp/Murcia_2021-04-20.jpeg", 
     "next": "tmp/Murcia_2021-03-26.jpeg",
-
     }
 
 with open(results_filename, mode="w", encoding="utf-8") as results:
     results.write(results_template.render(context))
-    print(f"... wrote {results_filename}")
+    print(f"generated: {results_filename}")
