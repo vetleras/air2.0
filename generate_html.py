f23ff59b919be343060902d61f7be64aad5c9bc5
Even Kjæxli <112700245+evenkjaexli@users.noreply.github.com>
2023-03-22 14:40:56 +0100

    added loading gif

--- generate_html.py
+++ generate_html.py
@@ -5,13 +5,14 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 results_filename = "results.html"
 results_template = environment.get_template("results.html")
 context = {
+    "country" : "ES",
     "city": "Murcia",  
 
     "date_1" : "2021-03-26", 
     "date_2" : "2021-04-20", 
 
-    "conc_1" : 30, 
-    "conc_2" : 70, 
+    "conc_1" : 70, 
+    "conc_2" : 30, 
 
     "img_1" : "tmp/Murcia_2021-03-26.jpeg",
     "img_2" : "tmp/Murcia_2021-04-20.jpeg",
