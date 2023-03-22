b4c44cfd6d01821603f3d92b72f345a7376d9b37
Even Kjæxli <112700245+evenkjaexli@users.noreply.github.com>
2023-03-22 10:01:59 +0100

    jinja2 templates

---
+++ generate_html.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, FileSystemLoader
+
+max_score = 100
+test_name = "Python Challenge"
+students = [
+    {"name": "Sandrine",  "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+]
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+
+results_filename = "students_results.html"
+results_template = environment.get_template("results.html")
+context = {
+    "students": students,
+    "test_name": test_name,
+    "max_score": max_score,
+}
+with open(results_filename, mode="w", encoding="utf-8") as results:
+    results.write(results_template.render(context))
+    print(f"... wrote {results_filename}")
