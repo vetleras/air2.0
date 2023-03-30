2582c60c200c56f76d4d11f2be0092b60dfad614
vetleras <vetleras@gmail.com>
2023-03-30 22:04:13 +0200

    Complete refactor

---
+++ download_and_aggregate.py
@@ -0,0 +1,170 @@
+from dataclasses import dataclass
+from datetime import datetime, timezone, timedelta
+import json
+import os
+import pickle
+from typing import Optional, Tuple
+from geopy.geocoders import Nominatim
+from geopy.location import Location
+from sentinelsat import SentinelAPI
+from tqdm import tqdm
+import requests
+import csv
+
+
+@dataclass
+class Measurement:
+    sampling_point: str
+    value: int
+    datetime: datetime
+
+
+class City:
+    def __init__(self, name: str, country_code: str) -> None:
+        self.name = name
+        self.country_code = country_code
+        self.location: Optional[Tuple[float, float]] = None
+        self.measurements: list[Measurement] = []
+        self.capture_dates: list[datetime] = []
+        self.aggregates: list[Tuple[Measurement, datetime]] = []
+
+    def save(self):
+        with open("cities.pkl-hex", "a+") as file:
+            file.write(pickle.dumps(self).hex() + "\n")
+
+
+def get_location(city: str, country_code: str) -> Optional[Tuple[float, float]]:
+    location: Location = Nominatim(user_agent="NTNU TTK4852 group 1").geocode(
+        {"city": city, "country": country_code}
+    )
+    if location is not None:
+        return (location.latitude, location.longitude)
+
+
+def get_measurement_urls(city: str, country_code: str) -> list[str]:
+    url = "https://fme.discomap.eea.europa.eu/fmedatastreaming/AirQualityDownload/AQData_Extract.fmw"
+    response = requests.get(
+        url,
+        params={
+            "CountryCode": country_code,
+            "CityName": city,
+            "Pollutant": 6001,
+            "Year_from": 2013,
+            "Year_to": 2022,
+            "Source": "E1a",
+            "Output": "TEXT",
+        },
+    )
+    response.raise_for_status()
+    response.encoding = "utf-8-sig"  # to remove BOM at beginning of file
+    return response.text.split()
+
+
+def get_measurements(city: str, country_code: str) -> list[Measurement]:
+    measurements = []
+    for url in get_measurement_urls(city, country_code):
+        response = requests.get(url)
+        response.raise_for_status()
+        reader = csv.DictReader(response.text.splitlines())
+        for row in reader:
+            if row["Concentration"] == "":
+                continue
+            measurement = Measurement(
+                sampling_point=row["AirQualityStationEoICode"],
+                value=round(float(row["Concentration"])),
+                datetime=datetime.strptime(
+                    row["DatetimeBegin"], "%Y-%m-%d %H:%M:%S %z"
+                ),
+            )
+            measurements.append(measurement)
+    return measurements
+
+
+def get_capture_dates(lat: float, lon: float) -> list[datetime]:
+    products = SentinelAPI(
+        "vetlerastirsed-javru6-nixdAk",
+        "tirsed-javru6-nixdAk",
+        show_progressbars=False,
+    ).query(
+        area=f"{lat}, {lon}",
+        area_relation="Contains",
+        platformname="Sentinel-2",
+        producttype="S2MSI2A",
+        cloudcoverpercentage=(0, 30),
+    )
+    return [
+        product["beginposition"].replace(tzinfo=timezone.utc)
+        for product in products.values()
+    ]
+
+
+def aggregate(
+    capture_datetimes: list[datetime], measurements: list[Measurement]
+) -> list[Tuple[Measurement, datetime]]:
+    """
+    Return all (measurement, capture_datetime) where the measurement is the most recent one within 24 hours before the capture.
+    If there are multiple applicable measurements, then the largest one is chosen.
+
+    Subsequent captures at the same time are ignored.
+    """
+    # Aggregation strategy:
+    # Assume capture_datetimes sorted in ascending order and measurements sorted in descending order by datetime, value.
+    # Take the first (least recent) capture and pop measurements until the second to last occured after the capture.
+    # The last measurement is now the most recent before (or simultanious) the capture. Add this to result and repeat.
+    aggregates = []
+    measurements = sorted(
+        measurements, key=lambda m: (m.datetime, m.value), reverse=True
+    )
+    for capture_datetime in sorted(capture_datetimes):
+        if not measurements:
+            return aggregates
+
+        while len(measurements) > 1 and measurements[-2].datetime < capture_datetime:
+            measurements.pop()
+
+        measurement = measurements.pop()
+        if timedelta() < capture_datetime - measurement.datetime < timedelta(days=1):
+            aggregates.append((measurement, capture_datetime))
+    return aggregates
+
+
+def load_cities() -> list[City]:
+    if os.path.isfile("cities.pkl-hex"):
+        with open("cities.pkl-hex", "r") as file:
+            return [pickle.loads(bytes.fromhex(row)) for row in file]
+    else:
+        return []
+
+
+if __name__ == "__main__":
+    with open("cities.json", "r") as file:
+        city_bar = tqdm(json.load(file)[len(load_cities()) :], unit="city")
+
+    for country_code, city_name in city_bar:
+        city = City(city_name, country_code)
+
+        def describe(s: str):
+            city_bar.set_description(f"{city.country_code} {city.name} - {s}")
+
+        describe("getting location")
+        city.location = get_location(city_name, country_code)
+        if city.location is None:
+            describe("writing to file")
+            city.save()
+            continue
+
+        describe("downloading measurements")
+        city.measurements = get_measurements(city_name, country_code)
+        if not city.measurements:
+            describe("writing to file")
+            city.save()
+            continue
+
+        describe("downloading capture datetimes")
+        city.capture_dates = get_capture_dates(*city.location)
+
+        describe("aggregating")
+        city.aggregates = aggregate(city.capture_dates, city.measurements)
+
+        describe("writing to file")
+        city.save()
---
+++ generate_results.py
@@ -0,0 +1,140 @@
+import os
+from pyproj import Proj
+from datetime import datetime, timedelta
+import matplotlib.image
+from sentinelhub import (
+    CRS,
+    BBox,
+    DataCollection,
+    MimeType,
+    SentinelHubRequest,
+    bbox_to_dimensions,
+    SHConfig,
+)
+from tqdm import tqdm
+from download_and_aggregate import City, load_cities, Measurement
+from jinja2 import Template
+from unidecode import unidecode
+import pathlib
+
+
+def generate_bbox(lat: float, lon: float, d=9 * 10**3) -> str:
+    p = Proj("EPSG:3035", preserve_units=False)
+    x, y = p(lat, lon)
+    tl_lat, tl_lon = p(x - d, y + d, inverse=True)
+    br_lat, br_lon = p(x + d, y - d, inverse=True)
+    return BBox((tl_lon, tl_lat, br_lon, br_lat), crs=CRS.WGS84)
+
+
+def download_image(lat: float, lon: float, datetime: datetime, filename: str):
+    if os.path.isfile(filename):
+        return
+
+    config = SHConfig()
+    config.sh_client_id = "4dd7b50f-f4a0-452c-bb4b-28825691c7eb"
+    config.sh_client_secret = "dyHmnCao1{(_1^Re1]#M)>zP)fr.7rb}-U{q09RI"
+
+    bbox = generate_bbox(lat, lon)
+    request = SentinelHubRequest(
+        evalscript="""
+        //VERSION=3
+
+        function setup() {
+        return {
+            input: ["B02", "B03", "B04"],
+            output: { bands: 3 }
+        };
+        }
+
+        function evaluatePixel(sample) {
+        return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+        }
+        """,
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L2A,
+                time_interval=(datetime.date(), datetime.date() + timedelta(days=1)),
+            ),
+        ],
+        responses=[
+            SentinelHubRequest.output_response("default", MimeType.JPG),
+        ],
+        bbox=bbox,
+        size=bbox_to_dimensions(bbox, resolution=10),
+        config=config,
+    )
+    matplotlib.image.imsave(filename, request.get_data()[0])
+
+
+def normalize(s: str) -> str:
+    """
+    Replace all non-ascii characters with ascii characters and replace all non-alphanumerical characters with underscores
+    """
+    return "".join(c if c.isalnum() else "_" for c in unidecode(s))
+
+
+class Image:
+    def __init__(
+        self, city: City, datetime: datetime, measurement: Measurement
+    ) -> None:
+        self.filename = f"{city.country_code}-{normalize(city.name)}-{datetime}.jpg"
+        download_image(*city.location, datetime, "static/" + self.filename)
+        self.datetime = datetime
+        self.measurement = measurement
+
+
+def generate_result(
+    city: City,
+    bad_image: Image,
+    good_image: Image,
+    filename: str,
+    previous_result: str,
+    next_result: str,
+):
+    with open("template.html", "r") as file:
+        template: Template = Template(file.read())
+
+    with open("static/" + filename, mode="w") as file:
+        file.write(
+            template.render(
+                city=city,
+                bad_image=bad_image,
+                good_image=good_image,
+                previous_result=previous_result,
+                next_result=next_result,
+            )
+        )
+
+
+if __name__ == "__main__":
+    cities = load_cities()
+    bad_aggregates = sorted(
+        (
+            (city, measurement, capture_datetime)
+            for city in cities
+            for measurement, capture_datetime in city.aggregates
+        ),
+        key=lambda c: c[1].value,
+        reverse=True,
+    )[:25]
+    pathlib.Path.mkdir("static", exist_ok=True)
+
+    for i, (city, bad_measurement, bad_capture_datetime) in enumerate(
+        tqdm(bad_aggregates)
+    ):
+        bad_image = Image(city, bad_capture_datetime, bad_measurement)
+
+        good_measurement, good_capture_datetime = min(
+            (aggregate for aggregate in city.aggregates), key=lambda a: a[0].value
+        )
+        good_image = Image(city, good_capture_datetime, good_measurement)
+
+        generate_result(
+            city,
+            bad_image,
+            good_image,
+            filename=f"{i}.html",
+            previous_result=f"{i-1}.html" if i > 0 else None,
+            next_result=f"{i+1}.html" if i < len(bad_aggregates) - 1 else None,
+        )
+    print(f"file://{os.getcwd()}/static/0.html")
